26c469b8da28a77a86afafa7e9fb14668fbb6b12
David Bureš <buresdv@gmail.com>
2022-04-27 09:48:47 +0200

    Arguments were not working

--- Sources/DavidFoundation/Get Arguments.swift
+++ Sources/DavidFoundation/Get Arguments.swift
@@ -6,14 +6,11 @@
 //
 
 import Foundation
-
-public extension CommandLine {
+   
+/// Get arguments passed to the program, without the name of the program itself
+func getTerminalArguments() -> [String] {
+    var arguments: [String] = CommandLine.arguments
+    arguments.remove(at: 0)
     
-    /// Get arguments passed to the program, without the name of the program itself
-    func getArguments() -> [String] {
-        var arguments: [String] = CommandLine.arguments
-        arguments.remove(at: 0)
-        
-        return arguments
-    }
+    return arguments
 }
