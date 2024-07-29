38e8f482a432c9274939e612d84c22d60426da82
David Bureš <buresdv@gmail.com>
2024-07-30 00:03:36 +0200

    ~ macOS version checking in some functions

--- Sources/DavidFoundation/Extensions/String/LocalizedStringKey to String.swift
+++ Sources/DavidFoundation/Extensions/String/LocalizedStringKey to String.swift
@@ -5,6 +5,7 @@
 //  Created by David Bureš on 10.03.2024.
 //
 
+
 import Foundation
 import SwiftUI
 
@@ -12,16 +13,23 @@ public extension LocalizedStringKey
 {
     var stringKey: String?
     {
-        Mirror(reflecting: self).children.first(where: { $0.label == "key" })?.value as? String
+        return Mirror(reflecting: self).children.first(where: { $0.label == "key" })?.value as? String
     }
 
     func stringValue(locale: Locale = .current) -> String?
     {
-        guard let stringKey = stringKey else { return nil }
-        guard let language = locale.language.languageCode?.identifier else { return stringKey }
-        guard let path = Bundle.main.path(forResource: language, ofType: "lproj") else { return stringKey }
-        guard let bundle = Bundle(path: path) else { return stringKey }
-        let localizedString = NSLocalizedString(stringKey, bundle: bundle, comment: "")
-        return localizedString
+        if #available(macOS 13.0, *)
+        {
+            guard let stringKey = stringKey else { return nil }
+            guard let language = locale.language.languageCode?.identifier else { return stringKey }
+            guard let path = Bundle.main.path(forResource: language, ofType: "lproj") else { return stringKey }
+            guard let bundle = Bundle(path: path) else { return stringKey }
+            let localizedString = NSLocalizedString(stringKey, bundle: bundle, comment: "")
+            return localizedString
+        }
+        else
+        {
+            return "stringValue is available only in macOS 13 or newer"
+        }
     }
 }
